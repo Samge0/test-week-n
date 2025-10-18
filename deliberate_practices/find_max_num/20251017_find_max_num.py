88fb246f5bbfb3ae7398ad589741c918fc1f13c0
SamgeShao <samge720@gmail.com>
2025-10-18 18:34:22 +0100

    save practice: 20251017_find_max_num

--- deliberate_practices/find_max_num/20251017_find_max_num.py
+++ deliberate_practices/find_max_num/20251017_find_max_num.py
@@ -10,12 +10,13 @@
 构建训练函数
 构建测试函数
 """
-
 import os, math, time, torch, swanlab
+from turtle import forward
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+
 
 # 当前目录
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -24,18 +25,19 @@ MODEL_PATH = os.path.join(CURRENT_DIR, 'models_output/model_20251017.pth')
 
 # 训练配置
 train_config = {
-    "input_size": 6,
+    "input_size": 9,
     "epochs": 300,
     "batch_size": 40,
     "batch_total": 2000,
     "lr": 0.001,
-    "current_date": "20251016"
+    "current_date": "20251017"
 }
 
 # 模型类
 class Net(nn.Module):
-    def __init__(self, input_size) -> None:
-        super().__init__()
+    
+    def __init__(self, input_size):
+        super(Net, self).__init__()
         self.l = nn.Linear(input_size, input_size)
         self.loss = F.cross_entropy
         
@@ -43,7 +45,7 @@ class Net(nn.Module):
         x = self.l(x)
         return x if y is None else self.loss(x, y)
     
-# 生成模拟数据
+# 构建数据
 def generate_datas(input_size, total):
     x = np.random.random((total, input_size))
     y = np.argmax(x, axis=1)
@@ -54,8 +56,8 @@ def evaluate(model, input_size, total=500):
     model.eval()
     x, y = generate_datas(input_size, total)
     with torch.no_grad():
-        y_pred = model(x)
-        return y_pred.argmax(dim=1).eq(y).sum().item() / total
+        y_perd = model(x)
+        return (y_perd.argmax(dim=1) == y).sum().item() / total
     
 # 训练
 def train():
@@ -66,7 +68,7 @@ def train():
     lr = train_config["lr"]
     current_date = train_config["current_date"]
     
-    batch_max = math.ceil(batch_total / batch_size)
+    batch_max = math.ceil(batch_total/batch_size)
     
     train_x, train_y = generate_datas(input_size, batch_total)
     model = Net(input_size)
@@ -93,6 +95,7 @@ def train():
         for batch_idx in range(batch_max):
             start_i = batch_idx * batch_size
             end_i = min(start_i + batch_size, batch_total)
+            
             x = train_x[start_i:end_i]
             y = train_y[start_i:end_i]
             
@@ -100,6 +103,7 @@ def train():
             loss = model(x, y)
             loss.backward()
             optim.step()
+            
             loss_logs.append(loss.item())
             
         acc = evaluate(model, input_size)
